46b37ca98ab2320781836eb385f4693ac9e14548
github username <github email address>
2019-09-03 09:18:35 -0500

    done

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,17 @@
+// Insertion sort works by looking at each element within a list (starting with the second element) and comparing it with the item before. If the item before is larger, they are swapped. This continues until the item is smaller at which point we do the same for the next element in the list.
+
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let el = arr[i];
+    let j;
+
+    for (j = i - 1; j >= 0 && arr[j] > el; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = el;
+  }
+  return arr;
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,22 @@
+// Find the smallest element. Swap it with the first element.
+// Find the second smallest element. Swap it with the second element.
+// Find the third smallest element. Swap it with the third element.
+// Repeat finding the next smallest element and swapping it into the corresponding correct position till the array is sorted.
+
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  let length = arr.length;
+  arr.map((e, i) => {
+    let min = i; //store index of min value
+    for (let j = i + 1; j < length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j; //updating the index
+      }
+    }
+    if (i !== min) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
+    }
+  });
+  return arr;
 }
