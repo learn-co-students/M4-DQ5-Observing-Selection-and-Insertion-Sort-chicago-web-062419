96327ee425bc502a08e872e8849b9551041cc7e4
srwatlanta <shawnrwheeler@gmail.com>
2019-09-03 10:16:43 -0500

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,14 @@
 function insertionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  function sort(arr){
+    for(let i = 1; i < arr.length; i++){
+      for(let x = i; x > 0; x--){
+        if(arr[x] < arr[x-1]){
+          arr.splice(x-1, 0, arr[x])
+          arr.splice(x+1, 1)
+          }
+        }
+      }
+    }
+  sort(arr)
+  return arr
 }
